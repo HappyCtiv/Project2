d9528242f818fa9a547f7556f07ef0211816cac3
Захар Шейко <zahar.sheyko@knights.ucf.edu>
2022-03-06 17:04:37 -0500

    CommitWithAsset

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 3f;
+
+    public void TakeDamage (float Damage)
+    {
+        hitPoints -= Damage;
+
+        if (hitPoints<=0)
+        {
+           Debug.Log("You Dead");
+        }
+    } 
+
+}
